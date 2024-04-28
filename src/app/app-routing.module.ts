3c6d3d9a2e6fa6f673ff9e19d0ac4de0e3f623cd
mudan <myudanov223@gmail.com>
2024-04-28 17:05:42 +0300

    added components for user but with not likes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,20 @@ import {LoginComponent} from './auth/login/login.component';
 import {RegisterComponent} from './auth/register/register.component';
 import {AuthGuardService} from "./helper/auth-guard.service";
 import {IndexComponent} from "./layout/index/index.component";
+import {AddPostComponent} from "./user/add-post/add-post.component";
+import {UserPostsComponent} from "./user/user-posts/user-posts.component";
+import {ProfileComponent} from "./user/profile/profile.component";
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'main', component: IndexComponent, canActivate: [AuthGuardService]},
+  {
+    path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService], children: [
+      {path: '', component: UserPostsComponent, canActivate: [AuthGuardService]},
+      {path: 'add', component: AddPostComponent, canActivate: [AuthGuardService]}
+    ]
+  },
 ];
 
 
